7046134d67351da8f17b762bc3d2e4f846b3df7a
Nathan Adler <nathan.adler@createunsw.com.au>
2016-06-12 03:14:27 +1000

    Added course line edge detection - still need to make usable. Added image-by-image user controlled iteration for analysis.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 // C and C++ headers
 #include <iostream>
 #include <sys/time.h>
+#include <cstdlib>
 
 // OpenCV libraries
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+// Define if using a normal camera instead of raspberry pi cam
+//#define USB_WEBCAM
+
+// Define if processing using still images instead of video
+#define STILL_IMAGES
+
+#ifndef USB_WEBCAM
 // Raspicam CV
 #include <raspicam/raspicam_cv.h>
+#endif
 
 // GPIO and Arduino-like functions
 #include <wiringPi.h>
@@ -17,26 +26,33 @@
 #include "DriveControl.hpp"
 
 // Image width and height constants
-const int iw = 640;
-const int ih = 480;
+static const int iw = 320;
+static const int ih = 240;
 
 using namespace std;
 using namespace cv;
 
-// define camera_t as raspberry pi cv camera class
-typedef raspicam::RaspiCam_Cv camera_t;
+static string image_path = "/home/pi/droid-racing/sample_images/";
 
+// define camera_t as raspberry pi cv camera class (or usb webcam)
+#ifndef USB_WEBCAM
+	typedef raspicam::RaspiCam_Cv camera_t;
+#else
+	typedef VideoCapture camera_t;
+#endif
+
+void detect_path(Mat grey);
 void detect_obstacles(Mat hsv, vector<Rect2i> & obj);
 void camera_setup(camera_t & cam);
 void sleep(double seconds);
 
 int main(int argc, char * argv[])
-{
-	// Instantiate camera object
-	camera_t cam;
-	
+{	
 	// Mat variables for storage of images
-	Mat im, imSmall, imHSV;
+	Mat im, imHSV, imGrey;
+
+	// Image channels for better analysis
+	vector<Mat> channels(3);
 
 	// Vector for storing locations of objects
 	vector<Rect2i> obstacles;
@@ -45,17 +61,26 @@ int main(int argc, char * argv[])
 	// bottom half of image
 	Rect2i ROI = Rect2i(0, ih/2, iw, ih/2);
 
-	// Initialise camera configuration before opening
-	camera_setup(cam);
+	#ifndef STILL_IMAGES
+		// Instantiate camera object
+		camera_t cam;
 
-	// Open camera
-	cam.open();
-	if (!cam.isOpened()){
-		return -1;
-	}
+		// Initialise camera configuration before opening
+		camera_setup(cam);
+
+		// Open camera
+		cam.open(0);
+		if (!cam.isOpened()){
+			return -1;
+		}
+	#endif
+
+	// variables for iterating through sample images
+	stringstream filename;
+	int imIndex = 462; // first jpeg in samples folder has index 0462
 
 	// Instantiate drive signals
-	DriveControl control;
+	//DriveControl control;
 
 	// Variable for timing
 	uint32_t loopTime;
@@ -67,12 +92,49 @@ int main(int argc, char * argv[])
 		loopTime = millis();
 
 		// get next image
-		cam.grab();
-		cam.retrieve(im);
+		#ifndef STILL_IMAGES
+			// if using camera, grab image
+			cam.grab();
+			cam.retrieve(im);
+
+		#else
+			// if using sample images, load next image
+
+			// clear filename
+			filename.clear();
+			filename.str(string());
+
+			// form filename from stubs
+			filename << image_path << "IMG_0" << imIndex << ".jpg";
+			cout << "Reading " << filename.str() << endl;
+
+			// read file
+			im = imread(filename.str());
+			if(im.data==NULL){
+				// exit on end
+				cout << "Reached end of images" << endl;
+				return 0;
+			}
+			// increment file index
+			imIndex++;
+		#endif
+
+		// currently resize to 320x240 because webcam won't let me read at that
+		resize(im, im, Size(iw,ih), 0, 0, CV_INTER_LINEAR);
 
 		// convert image to HSV for processing
 		cvtColor(im, imHSV, COLOR_BGR2HSV);
 
+		/**COLOUR LINE METHOD 1**/		
+		// split HSV channels
+		//split(imHSV, channels);
+		//detect_path(channels[1]);
+
+		/**COLOUR LINE METHOD 2 - THE BEST CURRENTLY**/
+		// convert hsv image to bgr->greyscale for processing
+		cvtColor(imHSV, imGrey, COLOR_BGR2GRAY);
+		detect_path(imGrey);
+		
 		// measure HSV colour at the centre of the image, for example
 		//cout << imHSV.at<Vec3b>(imHSV.size().width/2,imHSV.size().height/2) << endl;
 
@@ -83,20 +145,48 @@ int main(int argc, char * argv[])
 			el.y += ROI.y;
 			rectangle(im, el, Scalar(255,0,0));
 		}
-		
+
+		// print out info
+		cout << "Image size: " << im.size() << " Loop time: " << millis() - loopTime << endl;
+
 		// display image on screen
 		imshow("camera", im);
 
 		// allow for images to be displayed on desktop application
-		waitKey(1);
+		#ifdef STILL_IMAGES
+			cout << "Press any key for next image" << endl;
+			// need to be clicked in to one of the image windows (not terminal) for this to work
+			waitKey();
+		#else
+			waitKey(5);
+		#endif
 
 		// clear vector after use
 		obstacles.clear();
-
-		// print out info
-		cout << "Image size: " << im.size() << " Loop time: " << millis() - loopTime << endl;
 	}
 	return 0;
+}
+
+void detect_path(Mat grey)
+{
+	// get edge binary mask from grey image
+	Mat edges;
+	/** PLEASE READ UP ON CANNY **/
+	Canny(grey, edges, 60, 180); // note edge thresholding numbers
+
+	// show binary mask
+	imshow("Canny", edges);
+
+	// get approximate contours from binary mask
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(edges, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0,0));
+
+	vector<vector<Point>> contours_poly(contours.size());
+	for(size_t i = 0; i < contours.size(); ++i){
+		approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true);
+	}
 }
 
 void detect_obstacles(Mat hsv, vector<Rect2i> & obj)
@@ -104,7 +194,7 @@ void detect_obstacles(Mat hsv, vector<Rect2i> & obj)
 	Mat mask;
 
 	// generate mask of purple colours
-	inRange(hsv, Scalar(100, 65, 40), Scalar(140, 255, 255), mask);
+	inRange(hsv, Scalar(110, 100, 80), Scalar(130, 255, 255), mask);
 
 	// eliminate noise
 	int n = 2;
@@ -156,9 +246,16 @@ void camera_setup(camera_t & cam)
 	*/
 	
 	// For example
-	cam.set(CV_CAP_PROP_FORMAT, CV_8UC3);
-	cam.set(CV_CAP_PROP_FRAME_WIDTH, iw);
-	cam.set(CV_CAP_PROP_FRAME_HEIGHT, ih);
+	#ifndef USB_WEBCAM
+		cam.set(CV_CAP_PROP_FORMAT, CV_8UC3);
+		cam.set(CV_CAP_PROP_FRAME_WIDTH, iw);
+		cam.set(CV_CAP_PROP_FRAME_HEIGHT, ih);
+	#else
+		// does this work?
+		cam.set(CV_CAP_PROP_FRAME_WIDTH, iw);
+		cam.set(CV_CAP_PROP_FRAME_HEIGHT, ih);
+		cam.set(CV_CAP_PROP_EXPOSURE, 10);
+	#endif
 }
 
 /*
